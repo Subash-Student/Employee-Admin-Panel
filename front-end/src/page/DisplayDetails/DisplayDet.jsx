e71e2f614bd409a5e85f165c8899da01fe9623af
Subash <subashmurugan2021@gmail.com>
2024-08-21 21:08:56 +0530

    design employee details display

--- front-end/src/page/DisplayDetails/DisplayDet.jsx
+++ front-end/src/page/DisplayDetails/DisplayDet.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import "./display.css"
+
+
 
 const DisplayDet = () => {
   return (
-    <div>DisplayDet</div>
+    <div className='body'>
+
+    <div class="container">
+        <h2>Employee Details</h2>
+        <table>
+           
+            <tr>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Mobile Number</th>
+                <th>IT Designation</th>
+                <th>Gender</th>
+                <th>Degree</th>
+            </tr>
+            
+            <tr>
+                <td><img src={""} alt="Uploaded Image"/></td>
+                <td>John Doe</td>
+                <td>johndoe@example.com</td>
+                <td>+1234567890</td>
+                <td>Developer</td>
+                <td>Male</td>
+                <td>Bachelor's</td>
+            </tr>
+        </table>
+    </div>
+    </div>
   )
 }
 
 export default DisplayDet
+
+
